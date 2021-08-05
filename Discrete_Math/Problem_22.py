6a55dc1663fad9ff5cdf80d458f6fd8d2d37b382
rohan amin <rohan.amin@tophatmonocle.com>
2021-08-05 13:09:13 -0400

    .

--- Discrete_Math/Problem_22.py
+++ Discrete_Math/Problem_22.py
@@ -1,9 +1,20 @@
 # Problem 22: Are there positive integers a,b,c such that a ** 3 + b ** 3 = c ** 3?
 #             Are there positive integers a,c,b,d such that a ** 4 + b * 4 + c ** 4 = d ** 4
 
+def check_powerofthree():
+    for a in range(1000):
+        for b in range(1000):
+            for c in range(1000):
+                if a ** 3 + b ** 3 == c ** 3:
+                    print(a,b,c)
 
-for a in range(1000):
-    for b in range(1000):
-        for c in range(1000):
-            if a ** 3 + b ** 3 == c ** 3:
-                print(a,b,c)
+check_powerofthree()
+
+def check_poweroffour():
+    for a in range(1000):
+        for b in range(1000):
+            for c in range(1000):
+                if a ** 3 + b ** 3 == c ** 3:
+                    print(a,b,c)
+
+
